a1db5f5157b82f28cf04c9a1c079d5549567d54f
karola <deszcz.karolina@gmail.com>
2020-01-22 21:03:59 +0100

    finding best route from given point

--- routes_graph.py
+++ routes_graph.py
@@ -5,7 +5,7 @@ import os
 import networkx as nx
 
 from authors_graph import draw_graph, get_user_authority, build_users_graph
-from route_analysis import prepare_objects, find_route
+from route_analysis import prepare_objects, find_route, dist_obj_km
 from sentiment_analysis import get_thread_sentiment_score
 
 
@@ -88,6 +88,45 @@ def get_top_objects(graph):
     return high_sentiment_objects
 
 
+def BFS(graph, start, max_depth=5, max_distance=25):
+    visited_nodes = set()
+    queue = list()
+    queue.append(start)
+
+    parents = dict()
+    level = dict()
+    level[start] = 0
+    score = dict()
+    best_path_last_vertex = (start, 0)
+    while queue:
+        vertex = queue.pop(0)
+        parent_score = score[parents[vertex]] if vertex != start else 0
+        score[vertex] = parent_score + graph.nodes[vertex]['dict_attr']['total_score']
+        if score[vertex] > best_path_last_vertex[1]:
+            best_path_last_vertex = (vertex, score[vertex])
+        if vertex not in visited_nodes and level[vertex] < max_depth:
+            visited_nodes.add(vertex)
+            queue.extend(graph.neighbors(vertex))
+            for neighbour in graph.neighbors(vertex):
+
+                if neighbour not in visited_nodes and dist_obj_km(graph.nodes[neighbour]['dict_attr'],
+                                                                  graph.nodes[start]['dict_attr']) <= max_distance:
+                    parents[neighbour] = vertex
+                    level[neighbour] = level[vertex] + 1
+            print(vertex)
+
+    vertex_on_path = best_path_last_vertex[0]
+    path = list()
+    while vertex_on_path:
+        path.insert(0, vertex_on_path)
+        vertex_on_path = parents[vertex_on_path] if vertex_on_path != start else None
+
+    print(score)
+    print(path)
+
+    return path
+
+
 if __name__ == '__main__':
     x = build_routes_graph()
-    print((get_top_objects(x)))
+    BFS(x, 'Kuźnice')
